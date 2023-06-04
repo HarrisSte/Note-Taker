1ea30946c6f76df291b8301c3e5c384baccf2e16
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-06-04 09:37:58 -0400

    added routes folder

--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const uuid = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Middleware
+app.use(express.static('public'));
+
+//Listening for port
+app.listen(PORT, () =>
+  console.log(`App is listening at http://localhost:${PORT}`)
+);
